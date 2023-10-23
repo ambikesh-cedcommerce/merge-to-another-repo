53fccdc01e9acf0e5ce80c377285cd9f8b680924
Ambikesh Gautam-916 <65775577+ambikesh-cedcommerce@users.noreply.github.com>
2023-10-23 15:49:30 +0530

    Update Added-New-Feature.php

--- Added-New-Feature.php
+++ Added-New-Feature.php
@@ -1,4 +1,4 @@
 <?php
-echo "Added new feautures in the global settings hello more another text message added";
+echo "Added new feautures in the global settings hello more another text message added added";
 
 ?>
